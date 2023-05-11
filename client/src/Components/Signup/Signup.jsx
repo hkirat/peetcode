b65dbebfc7a004a066056dca5c610b50f2b6a903
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-05-11 20:58:23 +0530

    ui: change password input from text type to password type

--- client/src/Components/Signup/Signup.jsx
+++ client/src/Components/Signup/Signup.jsx
@@ -1,46 +1,60 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import "./Signup.css"
-import {backendUrl} from "../../constants.js";
+import "./Signup.css";
+import { backendUrl } from "../../constants.js";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div id="signup" className='flex-col'>
+    <div id='signup' className='flex-col'>
       <h1>Signup</h1>
       <div className='signup-form'>
+        <div className='subform'>
+          <label htmlFor='email'>Email: </label>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type='text'
+            name='email'
+            placeholder='Your Email'
+          />
+        </div>
 
         <div className='subform'>
-          <label htmlFor="email">Email: </label>
-          <input onChange={(e) => {
-            setEmail(e.target.value)
-          }} type="text" name='email' placeholder='Your Email' />
-        </div>
-        
-        <div className='subform'>
-          <label htmlFor="password">Password: </label>
-          <input onChange={(e) => setPassword(e.target.value)} type="text" name='password' placeholder='Your Password' />
+          <label htmlFor='password'>Password: </label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type='password'
+            name='password'
+            placeholder='Your Password'
+          />
         </div>
 
-        <button type="submit" id="test" onClick={async (e) => {
-          const response = await fetch(`${backendUrl}/signup`, {
-            method: "POST",
-            body: JSON.stringify({
-              email: email,
-              password: password
-            })
-          });
+        <button
+          type='submit'
+          id='test'
+          onClick={async (e) => {
+            const response = await fetch(`${backendUrl}/signup`, {
+              method: "POST",
+              body: JSON.stringify({
+                email: email,
+                password: password,
+              }),
+            });
 
-          const json = await response.json();
-          console.log(json);
-        }}>SIGNUP</button>
-        
+            const json = await response.json();
+            console.log(json);
+          }}
+        >
+          SIGNUP
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
 
 // neetcode1@gmail.com
