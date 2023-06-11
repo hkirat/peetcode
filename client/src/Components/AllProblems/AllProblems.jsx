a48161f37c552571a077653479de13268038cc27
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-06-11 15:36:50 +0530

    Code refactor, added loader skelton-component

--- client/src/Components/AllProblems/AllProblems.jsx
+++ client/src/Components/AllProblems/AllProblems.jsx
@@ -1,50 +1,50 @@
 import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 import "./AllProblems.css"
-import { backendUrl } from "../../constants.js";
+import {backendUrl} from "../../constants.js";
 
 const AllProblemsPage = () => {
-  const [problems, setProblems] = useState([]);
+    const [problems, setProblems] = useState([]);
 
-  const init = async () => {
-    const response = await fetch(`${backendUrl}/problems`, {
-      method: "GET",
-    });
+    const init = async () => {
+        const response = await fetch(`${backendUrl}/problems`, {
+            method: "GET",
+        });
 
-    const json = await response.json();
-    setProblems(json.problems);
-  }
+        const json = await response.json();
+        setProblems(json.problems);
+    }
 
-  useEffect(() => {
-    init()
-  }, []);
+    useEffect(() => {
+        init()
+    }, []);
 
-  return (
-    <div id="allproblems">
-      <table>
-        <tbody>
+    return (
+        <div id="allproblems">
+            <table>
+                <tbody>
 
-          <tr>
-            <th>Title</th>
-            <th>Difficulty</th>
-            <th>Acceptance</th>
-          </tr>
+                <tr>
+                    <th>Title</th>
+                    <th>Difficulty</th>
+                    <th>Acceptance</th>
+                </tr>
 
-          {problems.map((prob,index) => (
-            <tr>
-              <Link to={`/problems/:${prob.problemId}`}>
-                <td>{prob.title}</td>
-              </Link>
-              <td className={`${prob.difficulty}`} >{prob.difficulty}</td>
-              <td className={`${prob.difficulty}`} >{prob.acceptance}</td>
-            </tr>
-          ))}
+                {problems.map((prob, index) => (
+                    <tr>
+                        <Link to={`/problems/:${prob.problemId}`}>
+                            <td>{prob.title}</td>
+                        </Link>
+                        <td className={`${prob.difficulty}`}>{prob.difficulty}</td>
+                        <td className={`${prob.difficulty}`}>{prob.acceptance}</td>
+                    </tr>
+                ))}
 
-        </tbody>
-      </table>
-    </div>
-  )
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default AllProblemsPage
---
+++ client/src/Constants/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Loader.css'
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            Loading
+        </div>
+    )
+}
+
+export default Loader
